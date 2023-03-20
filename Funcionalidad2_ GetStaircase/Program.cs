becd2832160cb9598bb14d3954a5265b5e0a1316
AdrianRamirez23 <adrian.ramirez23@hotmail.com>
2023-03-19 19:07:57 -0500

    Add project files.

---
+++ Funcionalidad2_ GetStaircase/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Funcionalidad2__GetStaircase;
+
+List<string> listAgents = new List<string>() { "A", "B", "C" };
+string agent;
+int size = 0;
+
+Console.WriteLine("Seleccione A) Agente A. B) Agente B. C) Agente C: ");
+agent = Console.ReadLine();
+if (listAgents.Exists(a => a == agent.ToUpper()))
+{
+    Console.WriteLine("Ingrese el tamaño de lista de números (debe ser mayor a 0 y menor o igual a 100): ");
+    int.TryParse(Console.ReadLine(), out size);
+    if (size > 0 && size <= 100)
+    {
+        
+        Console.WriteLine("El agente seleccionado " + agent.ToUpper() + ", usó el metodo: " + new Staircase().ResultMethod(agent.ToUpper()) + ", y su resultado es: " );
+        new Staircase().GetStaircase(agent.ToUpper(), size);
+    }
+    else
+    {
+        Console.WriteLine("No elegiste un tamaño válido.");
+    }
+}
+else
+{
+    Console.WriteLine("No elegiste un agente válido.");
+}
+
---
+++ Funcionalidad2_ GetStaircase/Staircase.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funcionalidad2__GetStaircase
+{
+    public class Staircase
+    {
+        public void GetStaircase(string agent, int high)
+        {
+            switch (agent)
+            {
+                case "A":
+                    rigth(high);
+                    break;
+                case "B":
+                    left(high);
+                    break;
+                case "C":
+                    hexagonal(high);
+                    break;
+                
+            }
+        }
+        public string ResultMethod(string agent)
+        {
+            switch (agent)
+            {
+                case "A":
+                    return "Escalera alineada a la derecha";
+                    break;
+                case "B":
+                    return "Escalera alineada a la izquierda";
+                    break;
+                case "C":
+                    return "Escalera hexagonal";
+                    break;
+                default:
+                    return null;
+                    break;
+            }
+        }
+        internal void rigth(int high)
+        {
+            int start = high - 1, finish = 1;
+            bool endTask = false;
+
+            while (!endTask)
+            {
+                string result = string.Empty;
+                for (int i = 0; i < start; i++)
+                {
+                    if (i == 0)
+                    {
+                        result = "*";
+                    }
+                    else
+                    {
+                        result = result + "*";
+                    }
+                }
+                for (int j = 0; j < finish; j++)
+                {
+                    result = result + "#";
+                }
+                Console.WriteLine(result.Replace("*", " "));
+                start--;
+                finish++;
+                if (finish > high)
+                    endTask = true;
+            }
+        }
+        internal void left(int high) 
+        {
+            int start = 1;
+            bool endTask = false;
+
+            while (!endTask)
+            {
+                string result = string.Empty;
+                for (int i = 0; i < start; i++)
+                {
+                    if (i == 0)
+                    {
+                        result = "#";
+                    }
+                    else
+                    {
+                        result = result + "#";
+                    }
+                }
+               
+                Console.WriteLine(result);
+                start++;
+                if (start > high)
+                    endTask = true;
+            }
+        }
+        internal void hexagonal(int high) 
+        {
+            int start = high / 2, finish = high;
+            bool endTask = false;
+
+            while (!endTask)
+            {
+                string result = string.Empty;
+                for (int i = 0; i < start; i++)
+                {
+                    if (i == 0)
+                    {
+                        result = "*";
+                    }
+                    else
+                    {
+                        result = result + "*";
+                    }
+                }
+                for (int j = 0; j < finish; j++)
+                {
+                    result = result + "#";
+                }
+                Console.WriteLine(result.Replace("*", " "));
+                start--;
+                finish = finish + 2;
+                if (finish > high*2)
+                    endTask = true;
+            }
+            endTask= false;
+            finish = finish-3;
+            start = 1;
+            while (!endTask)
+            {
+                string result = string.Empty;
+                for (int i = 0; i < start; i++)
+                {
+                    if (i == 0)
+                    {
+                        result = "*";
+                    }
+                    else
+                    {
+                        result = result + "*";
+                    }
+                }
+                for (int j = 0; j < finish; j++)
+                {
+                    result = result + "#";
+                }
+                Console.WriteLine(result.Replace("*", " "));
+                start++;
+                finish = finish - 2;
+                if (finish < high)
+                    endTask = true;
+            }
+        }
+    }
+}
